a1f1a29439bdb0b9b267ef7637010192020971f2
DPK <35163005+CitizenSNPs@users.noreply.github.com>
2019-04-07 21:21:11 -0700

    Added ajax requests

--- calendar_app/front/calendarIndex.js
+++ calendar_app/front/calendarIndex.js
@@ -1,8 +1,44 @@
-import 'whatwg-fetch';
+$("document").ready(function() {
+  console.log('script loaded');
+})
 
-  const getEmployeeData = () => {
-    fetch('http://localhost:3000/calendar').then(response => {
-      console.log(response.json());
-    });
+//AJAX request to server; get request to grab employee data
+$("td").click(function(){
+  console.log(this.className);
+  var day = this.className.toString();
+  $.get('http://localhost:3000/calendar', function(data, status){
+    alert("Data: " + data + "\nStatus: " + status);
+    console.log(data);
+    for (index in data){
+      if(data[index]["schedule"]){
+        if (data[index]["schedule"].replace(/\s/g,'').split(",").includes(day)){ //trim spaces and split by comma
+          console.log('success');
+        }
+      }else{
+        console.log('failure');
+      }
+    }
+  });
+});
+
+$('#Next').click(function(){
+  if ($("p").html()=="December"){
+    console.log("Month is december");
+    return true;
   }
-}
+  const months = ["January", "February","March","April","May","June","July","August",
+  "September", "October", "November", "December"];
+  var index = months.indexOf($("p").html());
+  $("p").html(months[index+1]);
+});
+
+$('#Prev').click(function(){
+  if ($("p").html()=="January"){
+    console.log("Month is january");
+    return true;
+  }
+  const months = ["January", "February","March","April","May","June","July","August",
+  "September", "October", "November", "December"];
+  var index = months.indexOf($("p").html());
+  $("p").html(months[index-1]);
+});
--- calendar_app/index.js
+++ calendar_app/index.js
@@ -15,6 +15,12 @@ mongoose.connect('mongodb://localhost:27017/employees', { useNewUrlParser: true 
         console.log('Connected to Server successfully!');
     }
 });
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
