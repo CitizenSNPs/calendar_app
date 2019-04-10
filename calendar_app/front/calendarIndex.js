ad198f899cda6ca1ac253856674c5cc95ceb41b7
DPK <35163005+CitizenSNPs@users.noreply.github.com>
2019-04-09 17:02:59 -0700

    adding new employees from frontend

--- calendar_app/front/calendarIndex.js
+++ calendar_app/front/calendarIndex.js
@@ -1,10 +1,28 @@
 $("document").ready(function() {
   console.log('script loaded');
   inputDays();
+  createOptionMenu();
 });
 
-// // let firstDay = (new Date(2019,  )).getDay();
-// console.log(firstDay);
+//creating option menu
+const createOptionMenu = function(){
+  const select = document.getElementsByTagName('select');
+  $.get('http://localhost:3000/calendar', function(data, status){
+      console.log(data);
+      for (index in data){
+        if(data[index]["firstName"] && data[index]["lastName"] && data[index]["schedule"]){
+          let opt = document.createElement('option');
+          opt.value = data[index]["_id"];
+          opt.innerHTML = `${data[index]["firstName"]} ${data[index]["lastName"]}`;
+          select[0].appendChild(opt);
+
+        }else{
+          console.log(`Employee id: ${data[index]["_id"]} is not a valid employee`)
+        }
+      }
+    });
+}
+
 const months = ["January", "February","March","April","May","June","July","August",
 "September", "October", "November", "December"];
 const table = document.getElementById('calendar');
@@ -24,24 +42,50 @@ const inputDays = function(){
   $('td').eq(i).html(j+1) //adding days starting at first day of month
 }
 }
-//AJAX request to server; get request to grab employee data
+
+//AJAX request to server; GET request to grab employee data
 $("td").click(function(){
   console.log(this.className);
   var day = this.className.toString();
   $.get('http://localhost:3000/calendar', function(data, status){
-    alert("Data: " + data + "\nStatus: " + status);
     console.log(data);
     for (index in data){
       if(data[index]["schedule"]){
         if (data[index]["schedule"].replace(/\s/g,'').split(",").includes(day)){ //trim spaces and split by comma
-          console.log('success');
+          console.log(`${data[index]['firstName']} ${data[index]['lastName']}`);
         }
       }else{
-        console.log('failure');
+        console.log("day not in employee's schedule");
       }
     }
   });
 });
+
+//AJAX request to server; POST new employees to server
+$('#newEmployeeSubmit').click(function(){
+  if (!($('.formText')[0].value) || !($('.formText')[1].value)){
+    console.log('Could not create new employee: Please enter a valid first and last name');
+  } else if (!(checked())){
+    console.log('Could not create new employee: Please select a schedule.')
+  } else {
+  $.post('http://localhost:3000/calendar', { firstName: $('.formText')[0].value, lastName: $('.formText')[1].value, schedule:"Tues-Sat" } );
+  console.log('created a new employee')
+  }
+});
+
+//checking to make sure a schedule is chosen
+const checked = function(){
+  for (var i = 0; i < $('.newEmployeeCheckbox').length; i++){
+    if ($('.newEmployeeCheckbox')[i].checked){
+      return true
+    } else {
+      continue;
+    }
+  }
+  return false;
+}
+
+
 //next button
 $('#Next').click(function(){
   if ($("p").html()=="December"){
